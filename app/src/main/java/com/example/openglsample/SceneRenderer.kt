755a3b69b219875cb23010e297796ec7c2fd8dda
rohithsai45 <rohith@thesilverlabs.com>
2020-03-31 14:28:16 +0530

    adding modelview matrix along with model

--- app/src/main/java/com/example/openglsample/SceneRenderer.kt
+++ app/src/main/java/com/example/openglsample/SceneRenderer.kt
@@ -3,7 +3,10 @@ package com.example.openglsample
 import android.content.Context
 import android.opengl.GLES20
 import android.opengl.GLSurfaceView
+import android.renderscript.Float3
 import com.example.openglsample.models.Square
+import com.example.openglsample.shaderutil.ShaderProgram
+import com.example.openglsample.shaderutil.ShaderUtils
 import javax.microedition.khronos.egl.EGLConfig
 import javax.microedition.khronos.opengles.GL10
 
@@ -12,7 +15,13 @@ class SceneRenderer(private val context: Context) : GLSurfaceView.Renderer{
     private var square: Square? = null
 
     override fun onSurfaceCreated(gl: GL10?, config: EGLConfig?) {
-        square = Square(context)
+        val shader = ShaderProgram(
+            ShaderUtils.readShaderFileFromRawResource(context, R.raw.simple_vertex_shader),
+            ShaderUtils.readShaderFileFromRawResource(context, R.raw.simple_fragment_shader)
+        )
+        square = Square(shader)
+        val centerPosition = Float3(0.0f, 0.0f, 0.0f)
+        square?.setPosition(centerPosition)
     }
 
     override fun onSurfaceChanged(gl: GL10?, width: Int, height: Int) {
--- app/src/main/res/raw/simple_fragment_shader.glsl
+++ app/src/main/res/raw/simple_fragment_shader.glsl
@@ -1,5 +1,7 @@
 precision mediump float;
 
+varying lowp vec4 frag_Color;
+
 void main() {
-    gl_FragColor = vec4(1, 1, 1, 1);
+    gl_FragColor = frag_Color;
 }
